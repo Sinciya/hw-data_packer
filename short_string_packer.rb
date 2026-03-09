66c49c950115595900c3ee91ab662fca01eb346f
yingssin <ych45173929@gmail.com>
2026-03-09 19:06:27 +0800

    finished

--- short_string_packer.rb
+++ short_string_packer.rb
@@ -7,7 +7,9 @@ module ShortStringPacker
   #   str - String object
   # Returns: a Integer object
   def self.pack(str)
-    # IMPLEMENT THIS METHOD
+    list = str.split('')
+    .map {|char| (char.ord-96).to_s(2)}
+    .reduce(0){|acc, char| (acc << 5) + char.to_i(2)  }
   end
 
   ## Unpacks a Integer from pack() method into a short string
@@ -15,6 +17,15 @@ module ShortStringPacker
   #   packed - a Integer object
   # Returns: a String object
   def self.unpack(packed)
-    # IMPLEMENT THIS METHOD
+    str = packed.to_s(2)
+    if str.length > 5
+      time = str.length + 5 - (str.length % 5)
+      str = str.rjust(time, '0')
+    else
+      str = str.rjust(6-str.length, '0')
+    end
+    str.scan(/.{5}/).map {|char| (char.to_i(2)+96).chr}.join('')
+    
+
   end
 end
