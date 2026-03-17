49d22f81ec6888526c177c759fbe926b644d949e
yingssin <ych45173929@gmail.com>
2026-03-17 18:48:14 +0800

    fixed

--- short_string_packer.rb
+++ short_string_packer.rb
@@ -7,9 +7,9 @@ module ShortStringPacker
   #   str - String object
   # Returns: a Integer object
   def self.pack(str)
-    list = str.split('')
-    .map {|char| (char.ord-96).to_s(2)}
-    .reduce(0){|acc, char| (acc << 5) + char.to_i(2)  }
+    str.split('')
+       .map { |char| (char.ord - 96).to_s(2) }
+       .reduce(0) { |acc, char| (acc << 5) + char.to_i(2) }
   end
 
   ## Unpacks a Integer from pack() method into a short string
@@ -18,14 +18,17 @@ module ShortStringPacker
   # Returns: a String object
   def self.unpack(packed)
     str = packed.to_s(2)
-    if str.length > 5
-      time = str.length + 5 - (str.length % 5)
-      str = str.rjust(time, '0')
-    else
-      str = str.rjust(6-str.length, '0')
-    end
-    str.scan(/.{5}/).map {|char| (char.to_i(2)+96).chr}.join('')
-    
-
+    target_length = (str.length + 4) / 5 * 5
+    str.rjust(target_length, '0')
+       .scan(/.{5}/)
+       .map { |char| (char.to_i(2) + 96).chr }
+       .join
+    # if str.length > 5
+    #   time = str.length + 5 - (str.length % 5)
+    #   str = str.rjust(time, '0')
+    # else
+    #   str = str.rjust(6 - str.length, '0')
+    # end
+    # str.scan(/.{5}/).map { |char| (char.to_i(2) + 96).chr }.join('')
   end
 end
